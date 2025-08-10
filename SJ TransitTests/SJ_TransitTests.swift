ed849c68bf5e4bc9352893155bc916c55d5702ec
Vashishtha Jogi <vashishthajogi@gmail.com>
2025-08-10 09:06:08 -0700

    Add new test target

---
+++ SJ TransitTests/SJ_TransitTests.swift
@@ -0,0 +1,17 @@
+//
+//  SJ_TransitTests.swift
+//  SJ TransitTests
+//
+//  Created by Vashishtha Jogi on 8/10/25.
+//  Copyright © 2025 Vashishtha Jogi. All rights reserved.
+//
+
+import Testing
+
+struct SJ_TransitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
